5165a227fd142caa33e42a2cc31468469a7dc94e
Manuella Barros <manubarrossa@gmail.com>
2023-03-18 19:24:28 -0300

    Exercicio js com objeto e lista

--- Curso de JavaScript e TypeScript do básico ao avançado 2022/Seção 2 JavaScript Básico/Aula34. Exercício para função, array e objetos/js/script.js
+++ Curso de JavaScript e TypeScript do básico ao avançado 2022/Seção 2 JavaScript Básico/Aula34. Exercício para função, array e objetos/js/script.js
@@ -1,7 +1,36 @@
 function meuEscopo () { // função pra n haver conflitos de variaveis
     const form = document.querySelector('.form');
+    const res = document.querySelector('.resultado')
+    var listaPessoas = []; //Lista das pessoas
 
-    form.onsubmit = function (evento) {
-        alert(1);
-    };
+    function enviaDados (evento) {
+        evento.preventDefault(); // Faz o botão seguir oq eu colocar aqui
+        // ABAIXO pega as informações do form
+        var nome = form.querySelector('#nome').value;
+        var sobrenome = form.querySelector('#sobrenome').value;
+        var peso = form.querySelector('#peso').value;
+        var altura = form.querySelector('#altura').value;
+
+        //Adiciona na array oq for criado na função
+        listaPessoas.push(criaPessoa(nome, sobrenome, peso, altura)); 
+
+        console.log(listaPessoas);
+
+        res.innerHTML += `${nome} ${sobrenome} ${peso} ${altura} <br>`;
+
+        form.reset(); // Limpa formulário
+    }
+
+    function criaPessoa (nome, sobrenome, peso, altura) { // Cria um objeto pra cada pessoa inserida
+        return {
+            nome: nome,
+            sobrenome: sobrenome,
+            peso: peso,
+            altura: altura
+        }
+    }
+
+    form.addEventListener('submit', enviaDados);
 }
+
+meuEscopo();
